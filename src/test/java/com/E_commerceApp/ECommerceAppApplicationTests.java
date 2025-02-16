b46f6825cf7fa84523fde2872f0f12c065a79528
KaceGG <ndangkhoa567@gmail.com>
2025-02-16 22:18:23 +0700

    first commit

---
+++ src/test/java/com/E_commerceApp/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_commerceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
